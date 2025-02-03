306576c94bda1cd11ee6b9133c81a8ce1dcdc3a7
Ahmad Hassan <ahmadhassan4581947@gmail.com>
2025-02-03 11:29:32 +0500

    e commerace

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">
+                    Description
+                </div>
+                <div className="descriptionbox-nav-box fade">
+                    Reviews (122)
+                </div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates animi libero tenetur quasi? Magnam praesentium eum corporis! In, facere! Dolor sint reprehenderit vel voluptas tempore natus harum quo
+                     laboriosam, at pariatur quisquam ad nisi nesciunt deleniti, omnis excepturi vero aut ipsam impedit. Odio voluptatibus ipsum exercitationem nostrum, esse aperiam debitis, error facere natus provident aspernatur 
+                     nemo! Excepturi iste quae ipsam aut illo odio, voluptate pariatur eius ea ullam eaque reprehenderit minus? Officiis labore omnis, quos ipsam optio fugit quisquam, obcaecati, ipsum rem ex ducimus voluptatibus quia
+                      voluptas magni quasi temporibus voluptate aspernatur odit quaerat quae soluta quis nesciunt. Non voluptate maxime ullam reprehenderit odit iure maiores, velit nemo recusandae aliquam alias totam aperiam! Sapiente
+                       consequatur dolorum cupiditate, excepturi rem nulla asperiores aperiam enim temporibus cum non voluptatibus r tempora quae consequuntur ex necessitatibus ipsa repudiandae rerum. Dignissimos, </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis aut autem natus, ratione, labore aliquam blanditiis fuga impedit aspernatur incidunt veniam nulla alias explicabo similique nec accusantium sunt dolorem architecto
+                             id delectus, voluptates autem, ipsam molestiae. Cum sint eos doloribus enim ullam voluptates optio numquam, blanditiis nobis. Maxime, dolor? Quo quaerat accusantium natus, maxime, hic similique vel voluptatem molestiae nobis quasi sapiente quas corrupti!</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartItem from '../Components/CartItems/CartItem.jsx'
+
+const Cart = () => {
+  return (
+    <div>
+      <CartItem/>
+    </div>
+  )
+}
+
+export default Cart
